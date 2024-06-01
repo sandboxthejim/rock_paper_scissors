f4ef1a1c5f07b94fb31d6db5dcddcf229653e6ba
Jim <165657620+sandboxthejim@users.noreply.github.com>
2024-06-01 15:53:56 +0100

    Add script.js to index.html and add getComputerChoice function in js

---
+++ script.js
@@ -0,0 +1,15 @@
+//let userInput = promp('Rock, Paper or Scissors, let\'s go!'));
+
+const rock = 0;
+const paper = 1;
+const scissors = 2;
+let max = 3;
+function getComputerChoice(max){
+    return Math.floor(Math.random()*max);
+}
+if(getComputerChoice(max) == rock){
+    console.log('Rock');
+} else if (getComputerChoice(max) == paper){
+    console.log('Paper');
+} else {console.log('Scissors')};
+
