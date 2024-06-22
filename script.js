b1f5881286f992b22b277ffb23ef01dad99fa592
Jim <165657620+sandboxthejim@users.noreply.github.com>
2024-06-22 12:57:00 +0100

    Write the logic get the human choice

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//step 2 write the logic to get the computer choice
+//step 2 Write the logic to get the computer choice
 
 function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"];
@@ -6,3 +6,14 @@ function getComputerChoice(){
     return choices[randomChoices];
 }
 // console.log(getComputerChoice());
+
+//step 3 Write the logic to get the human choice
+
+function getHumanChoice(){
+    let userInput = prompt("Let's play rock-paper-scissors and see if you can beat the bot! Please choose between rock, paper, or scissors.").toLowerCase().trim();
+    while (userInput !== "rock" && userInput !== "paper" && userInput !== "scissors"){
+        userInput = prompt("Invaid input, please choose between rock, paper, or scissors.").toLowerCase().trim();
+    }
+    return userInput;
+}
+// console.log(getHumanChoice());
